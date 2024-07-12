b2f4cdd88fe655f1c12e0b01e0eae945fac7d43c
derya <derya.urcuk@ordina.nl>
2024-07-12 17:08:59 +0200

    Kleinde aanpassing

--- src/main/java/AnotherFile.java
+++ src/main/java/AnotherFile.java
@@ -2,7 +2,7 @@ public class AnotherFile {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 20; i++) {
+        for (int i = 1; i <= 25; i++) {
 
             System.out.println("Hello Tester3.. " + i);
         }
